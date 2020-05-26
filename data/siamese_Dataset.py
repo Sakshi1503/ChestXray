a2a8ee93d3112ae73b60ddf0e2ddb3bf910e5610
Sakshi <sakshij1503@gmail.com>
2020-05-26 11:52:16 +0530

    .

--- data/siamese_Dataset.py
+++ data/siamese_Dataset.py
@@ -51,6 +51,7 @@ class SiameseNetworkDataset():
                             '''
                 # labels = ([self.dict.get(n, n) for n in fields[5:]])
                 image_path = "/kaggle/input/chexpert/" + image_path[32:]
+                print(image_path)
                 image_two.append(image_path)
                 # assert os.path.exists(image_path), image_path
                 labels_two.append(labels)
