210cf1e47b8cf78b5c013a5965df0d20961fe7e8
Sakshi <sakshij1503@gmail.com>
2020-05-26 11:58:25 +0530

    .

--- data/siamese_Dataset.py
+++ data/siamese_Dataset.py
@@ -53,7 +53,6 @@ class SiameseNetworkDataset():
                 image_path = "/kaggle/input/chexpert/" + image_path[32:]
                 print(image_path)
                 image_two.append(image_path)
-                # assert os.path.exists(image_path), image_path
                 labels_two.append(labels)
                 '''
                 if flg_enhance and self._mode == 'train':
