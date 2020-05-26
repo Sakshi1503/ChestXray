ef8392d8b1b472498e075cff4410b4d677dc0168
Sakshi <sakshij1503@gmail.com>
2020-05-26 12:08:54 +0530

    .

--- data/siamese_Dataset.py
+++ data/siamese_Dataset.py
@@ -50,7 +50,7 @@ class SiameseNetworkDataset():
                             flg_enhance = True
                             '''
                 # labels = ([self.dict.get(n, n) for n in fields[5:]])
-                image_path = "/kaggle/input/chexpert/" + image_path[32:]
+                image_path = "/kaggle/input/chexpert/" + image_path[41:]
                 print(image_path)
                 image_two.append(image_path)
                 labels_two.append(labels)
